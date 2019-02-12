81e3d59745af50d8637100c3359dd9a2d05a6801
phantasm320 <xsportrudy@yahoo.com>
2019-02-12 16:49:31 -0600

    Created doc for function

--- lambdata_phantasm320/__init__.py
+++ lambdata_phantasm320/__init__.py
@@ -1,4 +1,13 @@
 def calculate(df_train, df_target, feature):
+    '''Function that returns a dictionary of the columns features
+       as a percentage to the target
+       ----------------------------------------------------------
+       df_train: is the name of your training data frame
+       df_target: is the name of your test data frame
+       feature: is the column from df_train that you want to
+                turn into a percentage
+    '''
+
     organize = {}
     for i in df_train[feature].value_counts().index:
         index_feature = df_train[df_train[feature] == i].index
