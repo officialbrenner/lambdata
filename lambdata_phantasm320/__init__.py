1d09f6396698f664518e9e39b333e593e6d926f6
phantasm320 <xsportrudy@yahoo.com>
2019-02-12 16:28:40 -0600

    Added new function

--- lambdata_phantasm320/__init__.py
+++ lambdata_phantasm320/__init__.py
@@ -1,2 +1,7 @@
-def tina_adds_stuff(x):
-    return x + 5
+def calculate(df_train, df_target, feature):
+    organize = {}
+    for i in df_train[feature].value_counts().index:
+        index_feature = df_train[df_train[feature] == i].index
+        percentages = df_target.loc[index_feature]['status_group'].value_counts().values  
+        organize[i] = round(percentages[0] / percentages.sum(), 3)
+    return organize
