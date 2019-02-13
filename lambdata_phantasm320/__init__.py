c5edf359ab1d05f62ab0a677e9add81e01e17e57
phantasm320 <xsportrudy@yahoo.com>
2019-02-13 09:15:53 -0600

    Added numpy pandas imports and more comments

--- lambdata_phantasm320/__init__.py
+++ lambdata_phantasm320/__init__.py
@@ -1,17 +1,25 @@
 def calculate(df_train, df_target, feature):
-    '''Function that returns a dictionary of the columns features
-       as a percentage to the target
-       ----------------------------------------------------------
-       df_train: is the name of your training data frame
-       df_target: is the name of your test data frame
-       feature: is the column from df_train that you want to
-                turn into a percentage
     '''
+    Function that returns a dictionary of the columns features
+    as a percentage to the target
+    -------------------------------------------------------------------
+    df_train(pandas dataframe): is the name of your training data frame
+    df_target(pandas dataframe): is the name of your test data frame
+    feature(column from df_train): is the column from df_train that you want to
+    turn into a percentage
+    '''
+    import numpy as np
+    import pandas as pd
 
-    organize = {} # create an empty dictionary
-    # Iterate through each unique feature in the column
-    for i in df_train[feature].value_counts().index: 
+    # create an empty dictionary to store percentages
+    organize = {} 
+    # Iterate through each unique name in the column
+    for i in df_train[feature].value_counts().index:
+        # Save and find the index for all instances of the unique name
         index_feature = df_train[df_train[feature] == i].index
-        percentages = df_target.loc[index_feature]['status_group'].value_counts().values  
+        # Search df_target for the index and return instances for each target
+        percentages = df_target.loc[index_feature]['status_group'].value_counts().values
+        # Add the name and highest instance into the dictionary
+        # as a percentage 
         organize[i] = round(percentages[0] / percentages.sum(), 3)
     return organize
