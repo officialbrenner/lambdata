7ce60acb1ee49bb68485b60e9f3fe6eb9c58658c
phantasm320 <xsportrudy@yahoo.com>
2019-02-13 09:46:29 -0600

    Fixed param names and test dataframe

--- lambdata_phantasm320/__init__.py
+++ lambdata_phantasm320/__init__.py
@@ -1,12 +1,12 @@
-def calculate(df_train, df_target, feature):
+def calculate(data, target, feature_1, feature_2):
     '''
-    Function that returns a dictionary of the columns features
-    as a percentage to the target
+    Returns a dictionary of a columns unique
+    names/values as a percentage with respect to the target
     -------------------------------------------------------------------
-    df_train(pandas dataframe): is the name of your training data frame
-    df_target(pandas dataframe): is the name of your test data frame
-    feature(column from df_train): is the column from df_train that you want to
-    turn into a percentage
+    data(pandas dataframe): is the name of your training data frame
+    target(pandas dataframe): is the name of your test data frame
+    feature_1(string): Column from data that you want to turn into a percentage
+    feature_2(string): Column from target that you want to test
     '''
     import numpy as np
     import pandas as pd
@@ -14,11 +14,11 @@ def calculate(df_train, df_target, feature):
     # create an empty dictionary to store percentages
     organize = {} 
     # Iterate through each unique name in the column
-    for i in df_train[feature].value_counts().index:
+    for i in data[feature_1].value_counts().index:
         # Save and find the index for all instances of the unique name
-        index_feature = df_train[df_train[feature] == i].index
+        index_feature = data[data[feature_1] == i].index
         # Search df_target for the index and return instances for each target
-        percentages = df_target.loc[index_feature]['status_group'].value_counts().values
+        percentages = target.loc[index_feature][feature_2].value_counts().values
         # Add the name and highest instance into the dictionary
         # as a percentage 
         organize[i] = round(percentages[0] / percentages.sum(), 3)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_phantasm320",
-    version="0.0.7",
+    version="0.0.8",
     author="phantasm320",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
